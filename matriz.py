f203edbcb88d61909449300b331796078d239a67
Nickkcj <nickcj123@hotmail.com>
2025-03-19 15:50:39 -0300

    add: matriz

---
+++ matriz.py
@@ -0,0 +1,13 @@
+class Matriz:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.matriz = [[0 for i in range(columns)] for j in range(rows)]
+
+    def set(self, row, column, value):
+        if row < 0 or row >= self.rows or column < 0 or column >= self.columns: raise Exception("Invalid row or column")
+        self.matriz[row][column] = value
+
+    def get(self, row, column):
+        if row < 0 or row >= self.rows or column < 0 or column >= self.columns: raise Exception("Invalid row or column")
+        return self.matriz[row][column]
