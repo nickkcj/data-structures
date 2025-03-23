004799d76f79387c9eae13a4a2bc9b37c5235eb0
Nickkcj <nickcj123@hotmail.com>
2025-03-23 17:18:38 -0300

    add: all DS to main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from DataStructures.matriz import SuperMatriz
 from DataStructures.array import SuperArray
 from DataStructures.filas import Fila
 from DataStructures.pilha import Pilha
+from DataStructures.binary_search_tree import BinarySearchTree
+from DataStructures.linked_list import LinkedList
 
 p = Pilha(5)
 p.push(2)
