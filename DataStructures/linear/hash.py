ebedd28e41c0d0dd450eeb1b6fcc235b38b706dc
Nickkcj <nickcj123@hotmail.com>
2025-04-23 21:03:41 -0300

    add: hash with module buckets

---
+++ DataStructures/linear/hash.py
@@ -0,0 +1,38 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [None] * size
+
+    def hash_function(self, key):
+        return key % self.size
+
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        if self.table[index] is None:
+            self.table[index] = []
+        self.table[index].append((key, value))
+
+    def search(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is not None:
+            for k, v in self.table[index]:
+                if k == key:
+                    return v
+        return None
+
+    def delete(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is not None:
+            for i, (k, v) in enumerate(self.table[index]):
+                if k == key:
+                    del self.table[index][i]
+                    return True
+        return False
+    
+
+    def display(self):
+        for i in range(self.size):
+            if self.table[i] is not None:
+                print(f"Index {i}: {self.table[i]}")
+            else:
+                print(f"Index {i}: Empty")
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from DataStructures.non_linear.binary_search_tree import BinarySearchTree
 from DataStructures.linear.linked_list import LinkedList
 from DataStructures.non_linear.graph import Graph
 from DataStructures.linear.linked_stack import LinkedStack
+from DataStructures.linear.hash import HashTable
 
 
 # # Example usage of LinkedQueue (Queue)
@@ -61,16 +62,39 @@ from DataStructures.linear.linked_stack import LinkedStack
 # l.dequeue()
 # l.print()
 
-g = Graph()
-g.add_edge(1, 2)
-g.add_edge(1, 3)
-g.add_edge(2, 4)
-g.add_edge(3, 4)
+# g = Graph()
+# g.add_edge(1, 2)
+# g.add_edge(1, 3)
+# g.add_edge(2, 4)
+# g.add_edge(3, 4)
 
-print("Neighbors of 1:", g.get_neighbors(1))
-print("Neighbors of 2:", g.get_neighbors(2))
-print("Neighbors of 3:", g.get_neighbors(3))
-print("Neighbors of 4:", g.get_neighbors(4))
+# print("Neighbors of 1:", g.get_neighbors(1))
+# print("Neighbors of 2:", g.get_neighbors(2))
+# print("Neighbors of 3:", g.get_neighbors(3))
+# print("Neighbors of 4:", g.get_neighbors(4))
 
-g.remove_edge(1, 2)
-print("Neighbors of 1 after removing edge (1,2):", g.get_neighbors(1))
+# g.remove_edge(1, 2)
+# print("Neighbors of 1 after removing edge (1,2):", g.get_neighbors(1))
+
+# Example usage of HashTable
+
+hash_table = HashTable(10)
+hash_table.insert(1, "one")
+hash_table.insert(2, "two")
+hash_table.insert(12, "twelve")
+hash_table.insert(22, "twenty-two")
+hash_table.insert(3, "three")
+hash_table.insert(13, "thirteen")
+hash_table.insert(23, "twenty-three")
+hash_table.insert(4, "four")
+hash_table.insert(14, "fourteen")
+hash_table.insert(24, "twenty-four")
+
+print("Search for key 1:", hash_table.search(1))
+print("Search for key 2:", hash_table.search(2))
+
+print("Search for key 12:", hash_table.search(12))
+
+print("Search for key 22:", hash_table.search(22))
+
+print(hash_table.display())
